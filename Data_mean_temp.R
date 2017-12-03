c7cb2ff5c2bf00dc0bec5b58d39e18b6face8731
Antonio <ntoniohu@gmail.com>
2017-12-03 13:51:17 +0100

    Full join of Kaunas and Madrid data. Year on one column and yearly means for each town in different columns

--- Data_mean_temp.R
+++ Data_mean_temp.R
@@ -1,3 +1,4 @@
+library("dplyr")
 data <- read.table("station_Madrid.txt", h=T)
 mean_data <-cbind(data$YEAR,data$metANN)
 colnames(mean_data) <- c("date", "temp")
@@ -5,7 +6,8 @@ mean_data <- mean_data[-1,] # removing the first year which is not measured prop
 
 only_months <- data[,1:13]
 
-write.table(mean_data, "data.tsv", sep="\t", row.names = F)
+# write.table(mean_data, "data.tsv", sep="\t", row.names = F)
+madrid_data <- mean_data 
 
 # Kaunas
 data <- read.table("station_Kaunas.txt", h=T)
@@ -14,6 +16,9 @@ colnames(mean_data) <- c("date", "temp")
 mean_data <- mean_data[-1,] # removing the first year which is not measured properly
 mean_data <- mean_data[-nrow(mean_data),] # removing the last year because it's not finished yet
 
-write.table(mean_data, "data.tsv", sep="\t", row.names = F)
+# write.table(mean_data, "data.tsv", sep="\t", row.names = F)
+kaunas_data <- mean_data
 
-
+dat <- full_join(as.data.frame(kaunas_data), as.data.frame(madrid_data), by = "date")
+colnames(dat)[2:3] <- c("kaunas", "madrid")
+write.table(dat, "data.tsv", sep="\t", row.names = F)
