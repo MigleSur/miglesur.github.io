2fa07529058e22197d079f9850409328448cb3dd
Antonio <ntoniohu@gmail.com>
2017-12-03 16:00:25 +0100

    Generate multiline plot showing data for Kaunas and Madrid. Based on https://bl.ocks.org/mbostock/3884955.

--- data_mean_temp.R
+++ data_mean_temp.R
@@ -20,7 +20,7 @@ mean_data <- mean_data[-nrow(mean_data),] # removing the last year because it's 
 kaunas_data <- mean_data
 
 dat <- full_join(as.data.frame(kaunas_data), as.data.frame(madrid_data), by = "date")
-colnames(dat)[2:3] <- c("kaunas", "madrid")
+colnames(dat)[2:3] <- c("Kaunas", "Madrid")
 
 dat <- dat[complete.cases(dat),]
 write.table(dat, "data.tsv", sep="\t", row.names = F, quote = F)
--- mean_temp_plot.js
+++ mean_temp_plot.js
@@ -1,43 +1,60 @@
 d3.select(window).on('load', init);
 
+var parseTime = d3.timeParse("%Y");
+
 function init() {
 
     var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 50},
+        margin = {top: 20, right: 40, bottom: 30, left: 50},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var parseTime = d3.timeParse("%Y");
 
-    var x = d3.scaleTime()
-        .rangeRound([0, width]);
-
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
+    var x = d3.scaleTime().range([0, width]);
+        y = d3.scaleLinear().range([height, 0]);
+        z = d3.scaleOrdinal(d3.schemeCategory10);
 
     var line = d3.line()
-        .x(function (d) {
-            return x(d.date);
-        })
-        .y(function (d) {
-            return y(d.temp);
+        // .curve(d3.curveBasis) // For smoothed lines
+
+        .x(function (d) { return x(d.date); })
+        .y(function (d) { return y(d.temperature); });
+
+    d3.tsv("data.tsv", type, function (error, data) {
+        if (error) throw error;
+        
+        var cities = data.columns.slice(1).map(function(id) {
+            return {
+                id: id,
+                values: data.map(function(d) {
+                    return {date: d.date, temperature: d[id]};
+                })
+            };
         });
 
-    d3.tsv("data_kaunas.tsv", function (d) {
-        d.date = parseTime(d.date);
-        d.temp = +d.temp;
-        return d;
-    }, function (error, data) {
-        if (error) throw error;
+        console.log(cities)
+        console.log(cities[0].values[0].temperature)
+        console.log(cities[0].values[1].temperature)
 
         x.domain(d3.extent(data, function (d) {
             return d.date;
         }));
+        
+        y.domain([
+            d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
+            d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
+        ]);
+
+        /*
         y.domain(d3.extent(data, function (d) {
             return d.temp;
         }));
+        */
 
+        z.domain(cities.map(function(c) { return c.id; }));
+ 
+        // X axis
         g.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x))
@@ -51,6 +68,7 @@ function init() {
             //.remove()
             ;
 
+        // Y axis
         g.append("g")
             .call(d3.axisLeft(y))
             .append("text")
@@ -61,6 +79,7 @@ function init() {
             .attr("text-anchor", "end")
             .text("Temp (°C)");
 
+        // Plot title
         g.append("text")
             .attr("x", (width / 2))
             .attr("y", 0 - (margin.bottom / 4 ))
@@ -69,15 +88,46 @@ function init() {
             .style('text-decoration','underline')
             .text("Yearly mean temperature in Kaunas");
 
-        g.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-linecap", "round")
-            .attr("stroke-width", 1.5)
-            .attr("d", line);
+        var city = g.selectAll(".city")
+            .data(cities)
+            .enter().append("g")
+            .attr("class", "city");
 
+        city.append("path")
+            .attr("class", "line")
+            .attr("d", function(d) { return line(d.values); })
+            .style("stroke", function(d) { return z(d.id); });
 
+        city.append("text")
+            .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+            .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+            .attr("x", 3)
+            .attr("dy", "0.35em")
+            .style("font", "10px sans-serif")
+            .text(function(d) { return d.id; });
     });
+    
+    
+//        // We dont know yet
+//        g.append("path")
+//            .datum(data)
+//            .attr("fill", "none")
+//            .attr("stroke", "steelblue")
+//            .attr("stroke-linejoin", "round")
+//            .attr("stroke-linecap", "round")
+//            .attr("stroke-width", 1.5)
+//            .attr("d", line);
+
 }
+
+function type(d, _, columns) {
+    d.date = parseTime(d.date);
+    // console.log(d)
+    for (var i = 1, n = columns.length, c; i < n; ++i) {
+        d[c = columns[i]] = +d[c];
+        // console.log(c)
+        // console.log(d[c])
+    }
+    return d;
+}
+
