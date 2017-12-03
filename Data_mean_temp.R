b6d0ba22e6ebe347710e29dcb4954854c48b208f
Migle <survilaite.migle@gmail.com>
2017-12-03 13:28:53 +0100

    some small changes, Kaunas data set

--- Data_mean_temp.R
+++ Data_mean_temp.R
@@ -7,4 +7,13 @@ only_months <- data[,1:13]
 
 write.table(mean_data, "data.tsv", sep="\t", row.names = F)
 
+# Kaunas
+data <- read.table("station_Kaunas.txt", h=T)
+mean_data <-cbind(data$YEAR,data$metANN)
+colnames(mean_data) <- c("date", "temp")
+mean_data <- mean_data[-1,] # removing the first year which is not measured properly
+mean_data <- mean_data[-nrow(mean_data),] # removing the last year because it's not finished yet
+
+write.table(mean_data, "data.tsv", sep="\t", row.names = F)
+
 
