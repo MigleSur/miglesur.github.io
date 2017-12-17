e1537c76d5b3a821785f20f60d5e209748f6da8f
Antonio <ntoniohu@gmail.com>
2017-12-17 13:24:44 +0100

    Support core interactivity. Click on a dot to display the corresponding hand with a nice transition. Mouse over a dot to make it bigger

--- pca_plot.js
+++ pca_plot.js
@@ -58,6 +58,11 @@ function init_plot1() {
             })
             .attr('r', 5)
             .attr('fill', 'black')
+            .attr("class", "dot")
+            .attr("id", function(d, i) {
+                return i;
+            })
+
             .append("svg:title")
             .text(function(d, i){i = +i; return "Hand: "+(i+1);})
     })
@@ -72,10 +77,10 @@ function init_plot2(){
     }
 
     var svg = d3.select('#plot2'),
-        margin = {top: 70, right: 30, bottom: 70, left: 30},
+        margin = {top: 70, right: 100, bottom: 70, left: 100},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        g = svg.append("g");
 
     var x = d3.scaleLinear()
         .range([0,width]);
@@ -83,36 +88,90 @@ function init_plot2(){
     var y = d3.scaleLinear()
         .range([height,0]);
 
+
+
+
+    var hands;
     d3.csv("hands.csv",
         function(d) {
         var i;
-        for (i=1; i<=57; i++) {
+        for (i=0; i<=56; i++) {
             d["X"+pad(i, 2)] = +d["X"+pad(i, 2)];
             d["Y"+pad(i, 2)] = +d["Y"+pad(i, 2)];
         }
         return d},
 
-        function(error, data){
+        function(error, data) {
             if (error) throw error;
-        var hands = d3.map(data);
-        var hand = hands.get("0");
-        var xval = d3.map(hand).values().slice(0,56);
-        var yval = d3.map(hand).values().slice(56,112);
 
-        var final = d3.zip(xval,yval);
+            var hands = d3.map(data);
 
-        var line = d3.line()
-            .x(function(d){return x(d[0]);})
-            .y(function(d){return y(d[1]);})
-            .curve(d3.curveCatmullRom)
+            g.append("circle")
+                .attr("transform", "translate(" + (30 + margin.left) + "," + margin.top + ")")
+                .attr("width", width)
+                .attr("height", height)
+                .attr("class", "outer_circle")
+                .attr("cx", width / 2)
+                .attr("cy", height / 2)
+                .attr("r", width / 1.2)
+                .attr("stroke", "black")
+                .attr("stroke-width", 2)
+                .attr("fill", "transparent");
 
-        g.append('path')
-            .attr('d', line(final))
-            .attr("fill", "#3c0500");
 
-        g.selectAll("line")
-            .data(final)
-            .enter()
+            g.append('path')
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .attr("class", "hand");
 
-        })
+
+            function draw_hand(k) {
+
+                var hand = hands.get(k);
+                var xval = d3.map(hand).values().slice(0, 56);
+                var yval = d3.map(hand).values().slice(56, 112);
+
+                var final = d3.zip(xval, yval);
+
+                var line = d3.line()
+                    .x(function (d) {
+                        return x(d[0]);
+                    })
+                    .y(function (d) {
+                        return y(d[1]);
+                    })
+                    .curve(d3.curveCatmullRom);
+
+                g.select('path').transition().duration(500)
+                    .attr('d', line(final))
+
+            }
+
+            draw_hand("0");
+
+
+            console.log("Hands");
+            console.log(hands);
+            d3.select("#plot1")
+                .selectAll("circle")
+                .on('click', function (d, i) {
+                    k = i.toString();
+                    // console.log(k);
+                    draw_hand(k);
+                })
+                .on('mouseover', function (d, i) {
+                    console.log(i);
+                    document.querySelectorAll(".dot")[i].setAttribute("r", 10);
+                })
+                .on('mousemove', function (d, i) {
+                    console.log(i);
+                    document.querySelectorAll(".dot")[i].setAttribute("r", 10);
+                })
+
+                .on('mouseout', function (d, i) {
+                    console.log(i);
+                    document.querySelectorAll(".dot")[i].setAttribute("r", 5);
+                });
+
+
+        });
 }
