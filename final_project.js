d28d9905d718efa228fccc780063a0f93bbc3bfa
Migle <survilaite.migle@gmail.com>
2018-01-06 16:01:51 +0100

    Added the interactivity to the first plot, needs transitions

--- final_project.js
+++ final_project.js
@@ -1,9 +1,13 @@
 
 // Initialiation function. Called after body has loaded
-function bar_plot() {
+function bar_plot(column_list, annotation_list, color_list, plotted_barplot) {
+
+    if(plotted_barplot) {
+        d3.select("#barplot").selectAll("*").remove()
+    }
 
     var barpl = d3.select("#barplot")
-    var margin = {top: 50, right: 20, bottom: 80, left: 20};
+    var margin = {top: 100, right: 20, bottom: 80, left: 20};
     var width = +barpl.node().getBoundingClientRect().width - margin.left - margin.right;
     var height = +barpl.node().getBoundingClientRect().height - margin.top - margin.bottom;
 
@@ -12,6 +16,8 @@ function bar_plot() {
 
     var x = d3.scaleBand().range([0, width]).padding(0.05),
         y = d3.scaleLinear().range([height, 0]);
+
+    var item;
 
     d3.tsv("data_discrimination.tsv", function(da) {
         da.widespread = +da.widespread;
@@ -45,126 +51,138 @@ function bar_plot() {
             .attr("y", -4)
             .text("%");
 
-        g.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("x", function(da) { return x(da.discrimination); })
-            .attr("y", function(da) { return y(da.widespread); })
-            .attr("fill", "#ffaf59")
-            .attr("width", width/34)
-            .attr("height", function(da) { return height - y(da.widespread);
-            });
+        // g.selectAll(".bar")
+        //     .data(data)
+        //     .enter().append("rect")
+        //     .attr("x", function(da) { return x(da.discrimination); })
+        //     .attr("y", function(da) { return y(da.widespread); })
+        //     .attr("fill", "#ffaf59")
+        //     .attr("width", width/34)
+        //     .attr("height", function(da) { return height - y(da.widespread);})
+        //     .append("title")
+        //     .text(function(da){ return da.widespread + "%"});
+        //     // .text(function(da,i){ console.log(da)});
+        //
+        // g.selectAll(".bar")
+        //     .data(data)
+        //     .enter().append("rect")
+        //     .attr("x", function(da) { return x(da.discrimination); })
+        //     .attr("y", function(da) { return y(da.rare); })
+        //     .attr("fill", "#4a99fb")
+        //     .attr("width", width/34)
+        //     .attr("transform", "translate("+width/34+")")
+        //     .attr("height", function(da) { return height - y(da.rare);})
+        //     .append("title")
+        //     .text(function(da){ return da.rare + "%"});
+        //
+        // g.selectAll(".bar")
+        //     .data(data)
+        //     .enter().append("rect")
+        //     .attr("x", function(da) { return x(da.discrimination); })
+        //     .attr("y", function(da) { return y(da.nonexistent); })
+        //     .attr("fill", "#f4214d")
+        //     .attr("width", width/34)
+        //     .attr("transform", "translate("+width*2/34+")")
+        //     .attr("height", function(da) { return height - y(da.nonexistent);})
+        //     .append("title")
+        //     .text(function(da){ return da.nonexistent + "%"});
 
-        g.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("x", function(da) { return x(da.discrimination); })
-            .attr("y", function(da) { return y(da.rare); })
-            .attr("fill", "#4a99fb")
-            .attr("width", width/34)
-            .attr("transform", "translate("+width/34+")")
-            .attr("height", function(da) { return height - y(da.rare);
-            });
+        // g.selectAll(".bar")
+        //     .data(data)
+        //     .enter().append("rect")
+        //     .attr("x", function(da) { return x(da.discrimination); })
+        //     .attr("y", function(da) { return y(da.dk); })
+        //     .attr("fill", "#1935f4")
+        //     .attr("width", width/34)
+        //     .attr("transform", "translate("+width*3/34+")")
+        //     .attr("height", function(da) { return height - y(da.dk);})
+        //     .append("title")
+        //     .text(function(da){ return da.dk + "%"});
+        for (item in column_list) {
+            g.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("x", function (da) {
+                    return x(da.discrimination);
+                })
+                .attr("y", function (da) {
+                    return y(da[column_list[item]]);
+                })
+                .attr("fill", color_list[item])
+                .attr("width", width / (8.5*column_list.length))
+                .attr("transform", "translate(" + width * item / (8.5*column_list.length) + ")")
+                .attr("height", function (da) {return height - y(da[column_list[item]]);})
+                .append("title")
+                .text(function (da) {
+                    return da[column_list[item]] + "%"
+                });
+        }
 
+        for (item in column_list) {
+            g.append("text")
+                .attr("x", 65)
+                .attr("y", -80 + (15) * item)
+                .attr("text-anchor", "start")
+                .style("font-size", "12px")
+                .text(annotation_list[item]);
 
+            // g.append("text")
+            //     .attr("x", 65)
+            //     .attr("y", -65)
+            //     .attr("text-anchor", "start")
+            //     .style("font-size", "12px")
+            //     .text("Rare");
+            //
+            // g.append("text")
+            //     .attr("x", 65)
+            //     .attr("y", -50)
+            //     .attr("text-anchor", "start")
+            //     .style("font-size", "12px")
+            //     .text("Non-existent");
+            //
+            // g.append("text")
+            //     .attr("x", 65)
+            //     .attr("y", -35)
+            //     .attr("text-anchor", "start")
+            //     .style("font-size", "12px")
+            //     .text("Don't know");
 
+            g.append("rect")
+                .attr("rx", 3)
+                .attr("ry", 3)
+                .attr("x", 47)
+                .attr("y", -90 + (15) * item)
+                .attr("width", 12)
+                .attr("height", 12)
+                .attr("fill", color_list[item])
 
-
-        g.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("x", function(da) { return x(da.discrimination); })
-            .attr("y", function(da) { return y(da.nonexistent); })
-            .attr("fill", "#f4214d")
-            .attr("width", width/34)
-            .attr("transform", "translate("+width*2/34+")")
-            .attr("height", function(da) { return height - y(da.nonexistent);
-            });
-
-        g.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("x", function(da) { return x(da.discrimination); })
-            .attr("y", function(da) { return y(da.dk); })
-            .attr("fill", "#1935f4")
-            .attr("width", width/34)
-            .attr("transform", "translate("+width*3/34+")")
-            .attr("height", function(da) { return height - y(da.dk);
-            });
-
-        // g.append("text")
-        //     .attr("x", (width / 2))
-        //     .attr("y", -10)
-        //     .attr("text-anchor", "middle")
-        //     .style("font-size", "14px")
-        //     .style('text-decoration','underline')
-        //     .text("Popularity of different types of discrimination");
-
-        g.append("text")
-            .attr("x", 65)
-            .attr("y", 10)
-            .attr("text-anchor", "start")
-            .style("font-size", "12px")
-            .text("Widespread");
-
-        g.append("text")
-            .attr("x", 65)
-            .attr("y", 25)
-            .attr("text-anchor", "start")
-            .style("font-size", "12px")
-            .text("Rare");
-
-        g.append("text")
-            .attr("x", 180)
-            .attr("y", 10)
-            .attr("text-anchor", "start")
-            .style("font-size", "12px")
-            .text("Non-existent");
-
-        g.append("text")
-            .attr("x", 180)
-            .attr("y", 25)
-            .attr("text-anchor", "start")
-            .style("font-size", "12px")
-            .text("Don't know");
-
-        g.append("rect")
-            .attr("rx", 3)
-            .attr("ry", 3)
-            .attr("x", 47)
-            .attr("y", -1)
-            .attr("width", 12.5)
-            .attr("height", 12.5)
-            .attr("fill", "#ffaf59")
-
-        g.append("rect")
-            .attr("rx", 3)
-            .attr("ry", 3)
-            .attr("x", 47)
-            .attr("y", 14)
-            .attr("width", 12.5)
-            .attr("height", 12.5)
-            .attr("fill", "#4a99fb");
-
-        g.append("rect")
-            .attr("rx", 3)
-            .attr("ry", 3)
-            .attr("x", 162)
-            .attr("y", -1)
-            .attr("width", 12.5)
-            .attr("height", 12.5)
-            .attr("fill", "#f4214d");
-
-        g.append("rect")
-            .attr("rx", 3)
-            .attr("ry", 3)
-            .attr("x", 162)
-            .attr("y", 14)
-            .attr("width", 12.5)
-            .attr("height", 12.5)
-            .attr("fill", "#1935f4");
-
-
+            // g.append("rect")
+            //     .attr("rx", 3)
+            //     .attr("ry", 3)
+            //     .attr("x", 47)
+            //     .attr("y", -75)
+            //     .attr("width", 12)
+            //     .attr("height", 12)
+            //     .attr("fill", "#4a99fb");
+            //
+            // g.append("rect")
+            //     .attr("rx", 3)
+            //     .attr("ry", 3)
+            //     .attr("x", 47)
+            //     .attr("y", -60)
+            //     .attr("width", 12)
+            //     .attr("height", 12)
+            //     .attr("fill", "#f4214d");
+            //
+            // g.append("rect")
+            //     .attr("rx", 3)
+            //     .attr("ry", 3)
+            //     .attr("x", 47)
+            //     .attr("y", -45)
+            //     .attr("width", 12)
+            //     .attr("height", 12)
+            //     .attr("fill", "#1935f4");
+        }
 
 
 
